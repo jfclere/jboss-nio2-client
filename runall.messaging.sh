0c1eac572a495dbc07b2e435f45d4bc5babf117d
Jean-Frederic Clere <jfclere@gmail.com>
2013-12-23 14:23:21 +0100

    Add configuration files and change logic to restart tomcat.

--- runall.messaging.sh
+++ runall.messaging.sh
@@ -1,13 +1,15 @@
 #
-# Don't forget to change httpd/ngnix/java located nearly at the end of the script.
+# Start the tomcats.
+start_tomcat()
+{
 for tc6 in 172.16.10.11 172.16.10.12 172.16.10.13 172.16.10.14 172.16.10.15
 do
   JVMROUTE=`ssh ${tc6} uname -n | awk -F . '{ print $1 }'`
+  ssh ${tc6} /tmp/tc6/bin/shutdown.sh
+  sleep 25
   ssh ${tc6} rm -rf /tmp/tc6
   ssh ${tc6} cp -r /home/jfclere/tc6 /tmp/tc6
   cat /home/jfclere/tc6/conf/server.xml | sed "s:NEO:$JVMROUTE:" > server.xml.tmp
-  ssh ${tc6} /tmp/tc6/bin/shutdown.sh
-  sleep 10
   scp server.xml.tmp ${USER}@${tc6}:/tmp/tc6/conf/server.xml
   ssh ${tc6} /tmp/tc6/bin/startup.sh
 done
@@ -22,6 +24,7 @@ do
     exit 1
   fi
 done
+}
 
 #for i in 5000 2500 2000 1000 833 666 500 416 384 357 333 294 263 250
 #for i in 225 220 180 150 130 100
@@ -30,6 +33,15 @@ done
 for i in 5000 2000 1000 833 666 500 416 384 357 333 294 263 250 225 220 180
 do
 
+  # restart the tomcats.
+  start_tomcat  
+
+  # check the server
+  curl -v http://172.16.10.8:8080/jboss-nio2-servlet/ 2>&1 | grep JSESSIONID
+  if [ $? -ne 0 ]; then
+    echo "172.16.10.8:8080 not reachable can't test"
+    exit 1
+  fi
   # messaging-17
   for node in messaging-23 messaging-20 messaging-19 messaging-18
   do
@@ -64,7 +76,7 @@ do
     echo $pid
     ssh messaging-08 kill -15 $pid
   fi
-  cp $$.top > $HOME/1500-$i-1000000-top-log.txt
+  cp $$.top $HOME/1500-$i-1000000-top-log.txt
   grep Cpu $$.top > $HOME/1500-$i-1000000-Cpu-log.txt
 
 done
